2b3d4e8620f7562882d6fdc70faeafb6df498714
akt <last.confessio.affections@gmail.com>
2026-06-15 16:27:16 +0900

    諸々調整、topにtrs-millionを追加

--- Practice03/eslint.config.mjs
+++ Practice03/eslint.config.mjs
@@ -14,6 +14,7 @@ export default [
       parserOptions: {
         parser: tseslint.parser,
         extraFileExtensions: ['.astro'],
+        tsconfigRootDir: import.meta.dirname,
       },
     },
   },
